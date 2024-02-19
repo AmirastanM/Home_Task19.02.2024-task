17044f58063f67d49f2518140c811d2f847a9250
AmirastanM <amirastanshm@code.edu.az>
2024-02-19 23:15:55 +0400

    added home task

---
+++ HomeTask19.02.2024/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+string ReversedName()
+{
+
+    Console.WriteLine("Write name: ");
+    string name = Console.ReadLine();
+    string CheckString (string name)
+    {
+        return name.Trim().ToLower();
+    }
+    string reversedText = "";
+
+
+    for (int i = name.Length - 1; i >= 0; i--)
+    {
+        reversedText += name[i];
+    }
+
+return reversedText;
+
+}
+Console.WriteLine(ReversedName());
